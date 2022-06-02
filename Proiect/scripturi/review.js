57735411dbe4da427167f4eff9c012f5ac496fa6
ldragosl <92057943+ldragosl@users.noreply.github.com>
2022-06-03 00:43:11 +0300

    Delete Proiect directory

--- Proiect/scripturi/review.js
+++
@@ -1,8 +0,0 @@
-class Review{
-    constructor(imagine_h, imagine_w, titlu, descriere){
-        this.imagine_h = imagine_h;
-        this.imagine_w = imagine_w;
-        this.titlu = titlu;
-        this.descriere = descriere;
-    }
-}
